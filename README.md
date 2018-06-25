f074c1b03f53b2f1dc8d9509ce543ca37830fe85
David Skrundz <david@skrundz.ca>
2018-06-25 17:22:49 -0600

    Add --keep-old to keep every version, and redo how arguments are parsed

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 A shell script that will bootstrap the 🅱️ compiler by incrementally building each version from `v0.0.1` to the latest. To update an existing compiler instead of rebuilding use the `--update` option.
 
 ```Bash
-./bootstrap.sh
+Usage: ./bootstrap.sh [argument, ...]
+Options:
+  -h, --help               Show help
+  --update                 Update existing B compiler
+  --keep-old               Preserve each build (eg. bc-0.0.1)
 ```
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,29 +3,72 @@
 major=0
 minor=0
 patch=0
+keep=0
+
+function usage {
+	echo "Usage: ./bootstrap.sh [argument, ...]"
+	echo "  -h, --help               Show help"
+	echo "  --update                 Update existing B compiler"
+	echo "  --keep-old               Preserve each build (eg. bc-0.0.1)"
+}
+
+function update {
+	if [ ! -e "bin/bc" ]
+	then
+		echo "No B compiler to update"
+		exit 1
+	fi
+	let major=`bin/bc --version | sed -nEe 's/^.*Version ([0-9]+)\.([0-9]+)\.([0-9]+).*$/\1/p'`
+	let minor=`bin/bc --version | sed -nEe 's/^.*Version ([0-9]+)\.([0-9]+)\.([0-9]+).*$/\2/p'`
+	let patch=`bin/bc --version | sed -nEe 's/^.*Version ([0-9]+)\.([0-9]+)\.([0-9]+).*$/\3/p'`
+}
+
+while [ "$1" != "" ]; do
+	PARAM=`echo $1 | awk -F= '{print $1}'`
+	VALUE=`echo $1 | awk -F= '{print $2}'`
+	case $PARAM in
+		-h | --help)
+			usage
+			exit 0
+			;;
+		--update)
+			update
+			;;
+		--keep-old)
+			let keep=1
+			;;
+		*)
+			echo "ERROR: unknown parameter \"$PARAM\""
+			usage
+			exit 1
+			;;
+	esac
+	shift
+done
 
 
-function checkVersion {
+
+function versionExists {
 	git rev-parse "v$major.$minor.$patch^{tag}" >/dev/null 2>&1
 	return $?
 }
 
 function nextVersion {
 	let patch+=1
-	if checkVersion
+	if versionExists
 	then
 		return $?
 	else
 		let patch=0
 		let minor+=1
-		if checkVersion
+		if versionExists
 		then
 			return $?
 		else
 			let patch=0
 			let minor=0
 			let major+=1
-			checkVersion
+			versionExists
 			return $?
 		fi
 	fi
@@ -43,11 +86,6 @@ function build {
 }
 
 
-if  [[ $1 = "--update" ]]; then
-	major=`bin/bc --version | sed -nEe 's/^.*Version ([0-9]+)\.([0-9]+)\.([0-9]+).*$/\1/p'`
-	minor=`bin/bc --version | sed -nEe 's/^.*Version ([0-9]+)\.([0-9]+)\.([0-9]+).*$/\2/p'`
-	patch=`bin/bc --version | sed -nEe 's/^.*Version ([0-9]+)\.([0-9]+)\.([0-9]+).*$/\3/p'`
-fi
 
 while nextVersion
 do
@@ -56,6 +94,10 @@ do
 	then
 		echo "Build failed"
 		exit 1
+	fi
+	if [ "$keep" = "1" ]
+	then
+		cp "bin/bc" "bin/bc-$major.$minor.$patch"
 	fi
 done
 
